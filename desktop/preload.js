86acfe2b9082b28f4f285fae1188ef60c1e8439c
Mineradio Developer <user@mineradio.local>
2026-07-27 15:35:44 +0800

    v1.0.1 - 本地模式纯净版：删除在线模式Home/更新按钮/登录系统/四大服务端模块

--- desktop/preload.js
+++ desktop/preload.js
@@ -8,10 +8,10 @@ contextBridge.exposeInMainWorld('desktopWindow', {
   exitFullscreenWindowed: () => ipcRenderer.invoke('desktop-window-exit-fullscreen-windowed'),
   getState: () => ipcRenderer.invoke('desktop-window-get-state'),
   close: () => ipcRenderer.invoke('desktop-window-close'),
-
-
-
-
+  openNeteaseMusicLogin: () => ipcRenderer.invoke('netease-music-open-login'),
+  clearNeteaseMusicLogin: () => ipcRenderer.invoke('netease-music-clear-login'),
+  openQQMusicLogin: () => ipcRenderer.invoke('qq-music-open-login'),
+  clearQQMusicLogin: () => ipcRenderer.invoke('qq-music-clear-login'),
   openUpdateInstaller: (filePath) => ipcRenderer.invoke('mineradio-open-update-installer', filePath),
   restartApp: () => ipcRenderer.invoke('mineradio-restart-app'),
   configureGlobalHotkeys: (bindings) => ipcRenderer.invoke('mineradio-hotkeys-configure-global', bindings || []),
